b2139a04c705b242c3a9bfbefe1cf7500240a615
Jesse <jesse.lee.smith@gmail.com>
2019-12-19 20:07:25 -0800

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
-
+  accepts_nested_attributes_for :user, reject_if: ->(attributes){ attributes['username'].blank? }, allow_destroy: true
 end
