eee51d8eed1b7a31ca852fe2d1ba64a485353846
Cristal Alatorre Perez <cristalalatorreperez@albert.home>
2019-12-26 20:41:06 -0800

    Done.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,4 +2,11 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
+  accepts_nested_attributes_for :user
+
+  def user_attributes=(user_attributes_hash)
+      self.user = User.find_or_create_by(username: user_attributes_hash[:username])  unless user_attributes_hash[:username].blank?
+  end
+
+
 end
